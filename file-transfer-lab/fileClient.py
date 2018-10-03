cf3ab264930fab9609f689df25ecb4542cbe98a1
Dani <danielagarciaher98@gmail.com>
2018-10-03 11:16:27 -0600

    sending and receiving file info implemented

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3
+
+# Echo client program
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
+
+from framedSock import framedSend, framedReceive
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+print("Enter file name: ")
+fileName = input()
+oFile = open(fileName, "rb")
+data = oFile.read()    
+
+framedSend(s, fileName.encode(), debug)
+
+
+i=0
+while i <= len(data):
+    str = data[i:i+100]        # open file duh not file name reds every 100 bytes
+    framedSend(s, str, debug)   #sends every 100 bytes
+    print("received:", framedReceive(s, debug))
+    i+=100
+
+framedSend(s, b"%%e", debug)  # signal end of input
---
+++ file-transfer-lab/fileServer.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+import sys, re, socket, os
+sys.path.append("../lib")       # for params
+import params
+os.chdir("sFol")
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+
+while True : 
+    print("listening on:", bindAddr)
+
+    sock, addr = lsock.accept()
+    fork = os.fork() 
+    if (fork == 0):
+        print("connection rec'd from", addr)
+
+
+        from framedSock import framedSend, framedReceive
+        fName = framedReceive(s, debug)     # Receive file name
+        oFile = open(fName, "wb")
+        while True:             # Once file transfer is done use system exit
+            payload = framedReceive(sock, debug)
+            if debug: print("rec'd: ", payload)
+            if not payload:
+                break
+            if b'%%e' in payload :
+                sys.exit(0)
+            payload += b"!"             # make emphatic!
+            framedSend(sock, payload, debug)
